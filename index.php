fb424856f82e81e023b617e77df2f512d84ae77a
jemmyx <meyer_fabio@hotmail.com>
2018-03-06 17:16:09 +0100

    calculated time period

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <?php
 $timer = (object)array(
-	'startdate' => '03/05/2018 11:59:59 PM',
-	'expdate' => '03/30/2018 11:59:59 AM',
+	// 'startdate' => '03/05/2018 11:59:59 PM',
+	// 'expdate' => '03/30/2018 11:59:59 AM',
+	'startdate' => strftime('%m/01/%Y 11:59:59 PM'),
+	'expdate' => strftime('%m/%d/%Y 11:59:59 AM', strtotime('+2 weeks')),
 );
+// var_dump($timer);exit;
 ?>
 <!doctype html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<title>Lab - Gauge</title>
 	<meta name="description" content="A simple standard gauge implementation">
 	<link rel="stylesheet" href="asset/css/styles.css">
@@ -21,7 +25,10 @@ $timer = (object)array(
 <body>
 	<div class="container">
 		<h1>Simple time-based gauge implementation</h1>
-		<p>@fabio meyer</p>
+		<div>
+			<p>Start date : <?php print $timer->startdate;?></p>
+			<p>End date : <?php print $timer->expdate;?></p>
+		</div>
 		<div id="bloc-progressbar">
 			<span id="countdown"></span>
 			<div id="progressbar"></div>
